657b9758de419b964bc7e73ece01db7ab3e75734
bvandepol <99882082+bvandepol@users.noreply.github.com>
2024-04-04 17:08:46 +0200

    Update Start-OSDCloud.ps1

--- Start-OSDCloud.ps1
+++ Start-OSDCloud.ps1
@@ -1,11 +1,11 @@
-Write-Host  -ForegroundColor Yellow "Starting Brooks' Custom OSDCloud ..."
+Write-Host  -ForegroundColor Yellow "Starting OSDCloud ..."
 cls
 Write-Host "===================== Main Menu =======================" -ForegroundColor Yellow
-Write-Host "================ www.brookspeppin.com =================" -ForegroundColor Yellow
-Write-Host "====================@brookspeppin =====================" -ForegroundColor Yellow
+Write-Host "==================== ID Logistics =====================" -ForegroundColor Yellow
+Write-Host "================== Beau van de Pol ====================" -ForegroundColor Yellow
 Write-Host "=======================================================" -ForegroundColor Yellow
-Write-Host "1: Zero-Touch Win10 21H1 | English | Enterprise"-ForegroundColor Yellow
-Write-Host "2: Zero-Touch Win10 20H2 | English | Enterprise" -ForegroundColor Yellow
+Write-Host "1: Windows 11 23H1 | English | Pro"-ForegroundColor Yellow
+Write-Host "2: Windows 10 20H2 | English | Pro" -ForegroundColor Yellow
 Write-Host "3: I'll select it myself"-ForegroundColor Yellow
 Write-Host "4: Exit`n"-ForegroundColor Yellow
 $input = Read-Host "Please make a selection"
@@ -17,8 +17,8 @@ Install-Module OSD -Force
 
 switch ($input)
 {
-    '1' { Start-OSDCloud -OSLanguage en-us -OSName 'Windows 10 22H2 x64' -OSEdition Pro -ZTI } 
-    '2' { Start-OSDCloud -OSLanguage en-us -OSName 'Windows 11 23H2 x64' -OSEdition Pro -ZTI } 
+    '1' { Start-OSDCloud -OSLanguage en-us -OSName 'Windows 11 23H2 x64' -OSEdition Pro -ZTI } 
+    '2' { Start-OSDCloud -OSLanguage en-us -OSName 'Windows 10 22H2 x64' -OSEdition Pro -ZTI } 
     '3' { Start-OSDCloud	} 
     '4' { Exit		}
 }
